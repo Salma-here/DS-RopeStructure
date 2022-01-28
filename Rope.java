47a83311c2367cacb48be1e940a4bbe358a85e12
Salma Erfani <salma.eztt@gmail.com>
2022-01-28 15:58:29 +0330

    split method in rope class

--- Rope.java
+++ Rope.java
@@ -115,6 +115,88 @@ public class Rope {
         return rope;
     }//TODO
 
+    public Node findSplitPoint(int index){ //only works for split point at the end of a string
+        Node node = root;
+        index++;
+        while(true){
+            int len = node.getLen();
+            if(index > len){
+                node = node.getRight();
+                index -= len;
+            }
+            else if(index < len){
+                node = node.getLeft();
+            }
+            else
+                return node; //send node to "splitByNode" method
+        }
+    }
+
+    public Node splitByNode(Node node){
+        //remove right and left link
+        Node rChild = node.getRight();
+        Node lChild = node.getLeft();
+        node.setRight(null);
+        //node.setLeft(null);?
+
+        Node rParent, lParent;
+        Node root = rChild;
+        //node = lChild;?
+
+        while(true){
+            lParent = nearestLeft(this.root, rChild);
+            if(lParent == null)
+                break;
+            root = lChild = lParent.getLeft();
+            lParent.setLeft(rChild);
+
+            rParent = nearestRight(this.root, lChild);
+            if(rParent == null)
+                break;
+            root = rChild = rParent.getRight();
+            rParent.setRight(lChild);
+        }
+
+        //todo
+        //one root is found and saved in "root"
+        //if given index is larger than original root's length,
+        //then "root" is root of other rope.
+        //if not, this.root should be replaced with "root"
+        return root; //temporary
+    }
+
+    public Node nearestRight(Node root, Node node){
+        //returns parent of the nearest ancestor to given node that is a RIGHT child
+        //base case
+        if(root == null)
+            return null;
+        Node rChild = root.getRight();
+        if(rChild.getLeft() == node || rChild.getRight() == node){
+            return root;
+        }
+        Node left = nearestRight(root.getLeft(), node);
+        if(left == null)
+            return nearestRight(root.getRight(), node);
+        else
+            return left;
+    }
+
+    public Node nearestLeft(Node root, Node node){
+        //returns parent of the nearest ancestor to given node that is a LEFT child
+        //base case
+        if(root == null)
+            return null;
+        Node lChild = root.getLeft();
+        if(lChild.getLeft() == node || lChild.getRight() == node){
+            return root;
+        }
+        Node left = nearestLeft(root.getLeft(), node);
+        if(left == null)
+            return nearestLeft(root.getRight(), node);
+        else
+            return left;
+    }
+
     public void insert(Rope rope, int index) {
         Rope lastRope = split(index);
         rope.concat(lastRope);
